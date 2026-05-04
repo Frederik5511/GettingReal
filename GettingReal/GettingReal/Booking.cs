6ac26c14241ec67c03adc7aa4f034f48d22f93a8
Rakul <rvan79142@edu.ucl.dk>
2026-05-04 13:12:30 +0200

    Opret booking

---
+++ GettingReal/GettingReal/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettingReal
+{
+    public class Booking
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Guests { get; set; }
+        public string Occasion { get; set; }
+
+        public Booking(string name, string phone, DateTime dateTime, int guests, string occasion)
+        {
+            Name = name;
+            Phone = phone;
+            DateTime = dateTime;
+            Guests = guests;
+            Occasion = occasion;
+        }
+    }
+}
